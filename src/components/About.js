051993bce9dc77353656b0bfe223cc503ab99d78
JenningsF <jenningsf@protonmail.com>
2023-02-13 22:54:38 -0600

    Added content in About component

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,13 @@ import React from "react";
 function About() {
 	return (
 		<section>
-			<h1>This is the About section</h1>
+			<h3>About</h3>
+			<p>
+				Howdy! I am developer learning the ins and outs of full stack
+				web development while also dabbling in security and DevOps. I am
+				a problem solver that strives for excellence and am always
+				looking to learn something new.
+			</p>
 		</section>
 	);
 }
