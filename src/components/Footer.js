0ca66a8c172c5eaec99062b302b877f6bac02f03
JenningsF <jenningsf@protonmail.com>
2023-02-14 22:36:03 -0600

    Added content in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 function Footer() {
 	return (
 		<section>
-			<h1>This is the Footer section</h1>
+			<a
+				href="https://www.linkedin.com/in/jenningsf/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FaLinkedinIn />
+			</a>
+			<a
+				href="https://www.twitter.com/jenningsf_dev/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FaTwitter />
+			</a>
+			<a
+				href="https://www.github.com/jenningsf/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				<FaGithub />
+			</a>
 		</section>
 	);
 }
