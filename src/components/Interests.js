88d29c5d911364dbdc7187208b9ecdf2a0ebba18
JenningsF <jenningsf@protonmail.com>
2023-02-14 22:21:44 -0600

    Added content in Interests component

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -3,7 +3,13 @@ import React from "react";
 function Interests() {
 	return (
 		<section>
-			<h1>This is the Interests section</h1>
+			<h3>Interests</h3>
+			<p>
+				Away from the keyboard I enjoy &#x1f4d6; reading, &#x1f3b9;
+				learning piano, &#x1f3cb; weightlifting and &#x1f33e;
+				homesteading with my family.
+			</p>
+			<p>Feel free to reach out! &#x1f44d;</p>
 		</section>
 	);
 }
